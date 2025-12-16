13dd033ae6b9f2224cb2ce29cdbbd41a8dcd8ecb
Jannik <raskob@fh-aachen.de>
2025-12-16 17:23:05 +0100

    organize imports

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
-
 from process import process_data
 
 app = FastAPI()
