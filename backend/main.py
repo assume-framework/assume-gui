efb7910c8d3415d32a93120a0fa3390892e71a39
Jannik <raskob@fh-aachen.de>
2025-12-16 17:18:17 +0100

    pass validation fails to frontend

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,23 @@
 from pathlib import Path
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 
-from .process import process_data
+from process import process_data
 
 app = FastAPI()
 
 
 @app.post("/api/submit")
 def send_data(data: dict):
-    world = process_data(data)
-    world.run()
+    try:
+        world = process_data(data)
+        world.run()
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
     return {"status": "success"}
 
 
--- src/sendData.tsx
+++ src/sendData.tsx
@@ -1,19 +1,24 @@
-import type { Node, Edge } from "@xyflow/react";
+import type {Node, Edge} from "@xyflow/react";
 
-async function sendData(nodes: Node[], edges: Edge[]){
-    const n = nodes.map(n => ({ id: n.id, type: n.type, data: n.data }));
-    const e = edges.map(e => ({ id: e.id, type: e.type, data: e.data }));
+async function sendData(nodes: Node[], edges: Edge[]) {
+    const n = nodes.map(n => ({id: n.id, type: n.type, data: n.data}));
+    const e = edges.map(e => ({id: e.id, type: e.type, data: e.data}));
 
     try {
-        await fetch('/api/submit', {
+        const resp = await fetch('/api/submit', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ nodes: n, edges: e }),
+            body: JSON.stringify({nodes: n, edges: e}),
         })
-    } catch (error) {
-        console.error("Error sending data:", error);
+        if (!resp.ok) {
+            const msg = await resp.json()
+            console.error("Received status code:", resp.status);
+            console.error(msg.detail);
+        }
+    } catch (e) {
+        console.error("Error when trying to submit data: ", e);
     }
 }
 
