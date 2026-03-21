6053c5c7aa68f3b5042ce01bb7fa296f222dc89d
Florian Maurer <maurer@fh-aachen.de>
2026-03-21 23:25:01 +0100

    add prettier and config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import {defineConfig, globalIgnores} from 'eslint/config'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['.venv', 'backend', 'node_modules']),
+  globalIgnores(['.venv', 'backend', 'node_modules', 'backend/static']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -16,11 +17,13 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn"
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+  // Must be last: turns off ESLint rules that conflict with Prettier
+  eslintConfigPrettier,
+]);
